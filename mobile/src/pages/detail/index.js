64c1eeaafc64dbdce3eca7ed1eeb463c502343a7
lais.siles <laislevine@gmail.com>
2020-03-31 22:55:39 -0300

    Parte 1º da Aplicação Mobile

--- mobile/src/pages/detail/index.js
+++ mobile/src/pages/detail/index.js
@@ -1,9 +1,86 @@
 import React from 'react';
-import {view, View} from 'react-native';
+import {View} from 'react-native';
+import styles from './styles'
+import logoimg from '../../assets/logo.png'
+import {Image, Text, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native'
+import {Feather} from '@expo/vector-icons';
+import * as MailComposer from 'expo-mail-composer';
+
+
 
 export default function Detail(){
+
+    const navigation = useNavigation();
+    const message = 'Olá, quero ajudar na caminha nova,'
+
+    function navigateBack(){
+        navigation.goBack();
+    }
+
+    function sendEmail(){       
+     MailComposer.composeAsync({
+        subject: "Heroi do caso: Caminha nova para zara",
+        recipients: ['laislevine@gmail.com'],
+        body: message,
+     })     
+
+    }
+
+
+    function sendWhatsapp(){
+
+
+    }
     return(
-        <View/>
+        <View style={styles.containner}>
+            <View style={styles.container}>
+            <View style ={styles.header}>
+                <Image source={logoimg}/>
+                
+               <TouchableOpacity onPress={navigateBack}>
+                <Feather name="arrow-left" size={28} color="#e02041"/>
+
+               </TouchableOpacity>
+            </View>
+
+            <View style={styles.incident}>
+            <Text style={styles.incidenPropety}>ONG</Text>
+                    <Text style={styles.incidenValue}>APAD</Text>
+
+                    <Text style={styles.incidenPropety}>CASO</Text>
+                    <Text style={styles.incidenValue}>Caminha nova pra Zara e Zeus</Text>
+
+                    <Text style={styles.incidenPropety}>VALOR:</Text>
+                    <Text style={styles.incidenValue}>100,00</Text> 
+           
+                  </View>  
+
+                  <View style={styles.contacBox}>
+                    <Text style={styles.heroTitle}> Salve o dia</Text>
+                    <Text style={styles.heroTitle}> Seja o herói desse caso!</Text>
+                    <Text style={styles.heroDescription}> Entre em contato</Text>
+
+                    <View style={styles.action}>
+
+                        <TouchableOpacity style={styles.actions} onPress={()=>{}}>
+                            <Text style={styles.actionText}>Whatsapp</Text>
+                        </TouchableOpacity>
+
+
+                        <TouchableOpacity style={styles.actions} onPress={sendEmail}>
+                            <Text style={styles.actionText}>Email</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                    </View>                 
+          
+            </View>
+           </View>
+
+
+
+        
     );
     
 }
--- mobile/src/pages/incidents/index.js
+++ mobile/src/pages/incidents/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import {view, View} from 'react-native';
+import {View, FlatList, Image, Text,TouchableOpacity } from 'react-native';
+import logoimg from '../../assets/logo.png'
+import styles from './styles';
+import {Feather} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native'
 
 export default function Incidents(){
+
+    const navigation= useNavigation();
+
+    function navigateToDetail(){
+        navigation.navigate('Detalis');
+
+    }
+
     return(
-        <View/>
+        <View style={styles.container}>
+            <View style ={styles.header}>
+                <Image source={logoimg}/>
+                <Text style={styles.headerText}>
+                    total de <Text style={styles.headerTextBold}> 0 casos</Text>.
+
+                </Text>
+            </View>
+
+            <Text style={styles.title}>Bem vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo</Text>
+
+                <FlatList
+                data={[1,2,3]}
+                style={styles.incidentsLits}
+                keyExtractor={incident =>String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={()=>(
+                    <View  style={styles.incident}>
+                    <Text style={styles.incidenPropety}>ONG</Text>
+                    <Text style={styles.incidenValue}>APAD</Text>
+
+                    <Text style={styles.incidenPropety}>CASO</Text>
+                    <Text style={styles.incidenValue}>Caminha nova pra Zara e Zeus</Text>
+
+                    <Text style={styles.incidenPropety}>VALOR:</Text>
+                    <Text style={styles.incidenValue}>100,00</Text>
+
+                   <TouchableOpacity 
+                   style={styles.detailsButton} 
+                   onPress={navigateToDetail}>
+                       <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                       <Feather name="arrow-right" size={16} color="#e02041"/>
+
+                   </TouchableOpacity >
+                </View>
+
+                )}
+                
+                />
+
+            
+         </View>
     );
     
 }
